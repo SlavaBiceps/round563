46da39fb870ffa8bac4bdce28f0f7fa8fb12e0ab
SlavaBiceps <slava_ivanov_cs_12321@mail.ru>
2019-06-20 11:49:48 +0500

    1

---
+++ a/main.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+    long long mas[2*n+2];
+    for (long long i=0;i<2*n;i++){
+        cin >> mas[i];
+    }
+    sort(mas,mas+2*n);
+    long long sum1=0,sum2=0;
+    for (long long i=0;i<n;i++){
+        sum1+=mas[i];
+    }
+    for (long long i=n;i<2*n;i++){
+        sum2+=mas[i];
+    }
+    cout << sum1 << " " << sum2 << endl;
+    if (sum1!=sum2){
+        for (long long i=0;i<2*n;i++){
+            cout << mas[i] << " ";
+        }
+    } else {
+        cout << "-1";
+    }
+}
